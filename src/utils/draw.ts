bf9250359f6962ea548b2f12e6f4f8237ce4f210
pengzhijian <13087545742>
2024-05-17 17:24:17 +0800

    新增图片上传回显功能，图片像素转换功能，调整像素宽高自动缩放功能

--- src/utils/draw.ts
+++ src/utils/draw.ts
@@ -5,15 +5,16 @@ import { type ReactPositionInfo } from '@/type/draw.type.ts'
  * @params { HTMLCanvasElement } canvas 画布对象
  * @params { number } width 画布宽度
  * @params { number } height 画布高度
- * @params { number } gridPerPx 每个格子的像素大小
+ * @params { number } gridWidth 格子宽度
+ * @params { number } gridHeight 格子高度
  * @params { string } lineColor 线条颜色
  * @params { boolean } showLine 是否显示间隔线条
  * @params { string } pixelColor 像素颜色
  * @params { number[][] } rectPosArr 存储格子坐标信息
 */
-export function initMap(canvas: HTMLCanvasElement,setting: { width: number, height: number, gridPerPx: number, lineColor: string, lineShow: boolean, rectPosArr?: ReactPositionInfo[][] }) {
+export function initMap(canvas: HTMLCanvasElement,setting: { width: number, height: number, gridWidth: number, gridHeight: number, lineColor: string, lineShow: boolean, rectPosArr?: ReactPositionInfo[][] }) {
   if (!canvas) return;
-  const { width, height, gridPerPx, lineColor, lineShow, rectPosArr } = setting;
+  const { width, height, lineColor, lineShow, rectPosArr, gridWidth, gridHeight } = setting;
   canvas.width = width;
   canvas.height = height;
   if (lineShow) {
@@ -22,18 +23,18 @@ export function initMap(canvas: HTMLCanvasElement,setting: { width: number, heig
     ctx.beginPath();
     
     // 格子数不一定能被整除，所以要补上缺的宽高使其能整除
-    const widthLineNum = canvas.width / gridPerPx - 1; // 横向线条数量
-    if (canvas.width % gridPerPx == 0) {
+    const widthLineNum = canvas.width / gridWidth - 1; // 横向线条数量
+    if (canvas.width % gridWidth == 0) {
       canvas.width += widthLineNum
     } else {
-      canvas.width += widthLineNum + (gridPerPx - (canvas.width % gridPerPx));
+      canvas.width += widthLineNum + (gridWidth - (canvas.width % gridWidth));
     }
     // 竖向线条数量
-    const heightLineNum = canvas.height / gridPerPx - 1;
-    if (canvas.height % gridPerPx == 0) {
+    const heightLineNum = canvas.height / gridHeight - 1;
+    if (canvas.height % gridHeight == 0) {
       canvas.height += heightLineNum;
     } else {
-      canvas.height += heightLineNum + (gridPerPx - (canvas.height % gridPerPx));
+      canvas.height += heightLineNum + (gridHeight - (canvas.height % gridHeight));
     }
   
     // 设置线条样式
@@ -43,14 +44,14 @@ export function initMap(canvas: HTMLCanvasElement,setting: { width: number, heig
     // 开始绘制横线
     for (let i = 0; i < heightLineNum; i++) {
       // 注意要算线的宽度，也就是后面那个+i
-      ctx.moveTo(0, gridPerPx * (i + 1) + i);
-      ctx.lineTo(canvas.width, gridPerPx * (i + 1) + i);
+      ctx.moveTo(0, gridHeight * (i + 1) + i);
+      ctx.lineTo(canvas.width, gridHeight * (i + 1) + i);
       ctx.stroke();
     }
     // 开始绘制竖线
     for (let i = 0; i < widthLineNum; i++) {
-      ctx.moveTo(gridPerPx * (i + 1) + i, 0);
-      ctx.lineTo(gridPerPx * (i + 1) + i, canvas.height);
+      ctx.moveTo(gridWidth * (i + 1) + i, 0);
+      ctx.lineTo(gridWidth * (i + 1) + i, canvas.height);
       ctx.stroke();
     }
   }
@@ -61,8 +62,7 @@ export function initMap(canvas: HTMLCanvasElement,setting: { width: number, heig
       if (item) {
         item.forEach(rect => {
           if (rect) {
-            console.log(rect)
-            drawRect(canvas, {hasLine: lineShow, x: rect.x, y: rect.y, gridPerPx, color: rect.color});
+            drawRect(canvas, {hasLine: lineShow, x: rect.x, y: rect.y, gridWidth: gridWidth, gridHeight: gridHeight, color: rect.color});
           }
         })
       }
@@ -76,12 +76,15 @@ export function initMap(canvas: HTMLCanvasElement,setting: { width: number, heig
  * @constructor
  * @params { HTMLCanvasElement } canvas 画布对象
  * @params { 'line' | 'pixel' } mode 画笔类型，线条或像素
- * @params { number } gridPerPx 每个格子的像素大小
+ * @params { number } gridWidth 每个格子的像素大小
+ * @params { string } lineColor 线条颜色
+ * @params { boolean } hasLine 是否显示间隔线条
+ * 
  * 
 */
-export function drawLineHandler(canvas: HTMLCanvasElement, setting: {gridPerPx: number, lineColor: string}) {
+export function drawLineHandler(canvas: HTMLCanvasElement, setting: {gridWidth: number, lineColor: string}) {
   if (!canvas) return;
-  const { gridPerPx, lineColor } = setting;
+  const { lineColor, gridWidth } = setting;
   let startMove = false; // 鼠标是否按下
   let lastX = 0; // 鼠标按下时的横坐标
   let lastY = 0; // 鼠标按下时的纵坐标
@@ -93,7 +96,7 @@ export function drawLineHandler(canvas: HTMLCanvasElement, setting: {gridPerPx: 
     lastY = e.offsetY;
     // 原处画一个点
     ctx.beginPath();
-    ctx.lineWidth = gridPerPx;
+    ctx.lineWidth = gridWidth;
     ctx.strokeStyle = lineColor;
     ctx.lineCap = "round"; // 线条两端为圆角
     ctx.moveTo(lastX, lastY);
@@ -120,6 +123,7 @@ export function drawLineHandler(canvas: HTMLCanvasElement, setting: {gridPerPx: 
   }
   canvas.addEventListener("mouseup", onMouseUpFun)
 
+  // 将注册事件返回，方便清除事件
   return {
     onMouseDownFun,
     onMouseMoveFun,
@@ -131,25 +135,27 @@ export function drawLineHandler(canvas: HTMLCanvasElement, setting: {gridPerPx: 
  * 画板注册画笔事件，添加绘画像素功能
  * @constructor
  * @params { HTMLCanvasElement } canvas 画布对象
- * @params { number } gridPerPx 每个格子的像素大小
+ * @params { number } gridWidth 每个格子的像素宽度
+ * @params { number } gridHeight 每个格子的像素高度
  * @params { string } lineColor 线条颜色
  * @params { boolean } hasLine 是否显示间隔线条
  * @params { string } pixelColor 像素颜色
  * @params { number[][] } rectPosArr 存储格子坐标信息
  * 
 */
-export function drawPixelHandler(canvas: HTMLCanvasElement, setting: { hasLine: boolean, gridPerPx: number, pixelColor: string, rectPosArr: (ReactPositionInfo | null)[][] }) {
-  const { hasLine, gridPerPx, pixelColor, rectPosArr } = setting;
+export function drawPixelHandler(canvas: HTMLCanvasElement, setting: { hasLine: boolean, gridWidth: number, gridHeight: number, pixelColor: string, rectPosArr: (ReactPositionInfo | null)[][] }) {
+  const { hasLine, pixelColor, rectPosArr, gridWidth, gridHeight } = setting;
   let startMove = false; // 鼠标是否按下
   let lastX = 0; // 鼠标按下时的横坐标
   let lastY = 0; // 鼠标按下时的纵坐标
-  const reactArr: {x:number, y:number}[] = [] // 用来存储本次落笔画了的格子
+  let reactArr: {x:number, y:number}[] = [] // 用来存储本次落笔画了的格子
   const onMouseDownFun = (e: MouseEvent) => {
     // 记录按下时的坐标
     startMove = true;
+    reactArr = []
     lastX = e.offsetX;
     lastY = e.offsetY;
-    const [gridX, gridY] = mouseToGrid({x:lastX, y:lastY, gridPerPx, hasLine});
+    const [gridX, gridY] = mouseToGrid({x:lastX, y:lastY, gridHeight, gridWidth, hasLine});
     // 将坐标信息存储到数组中
     if (!rectPosArr[gridX]) {
       rectPosArr[gridX] = []
@@ -157,11 +163,11 @@ export function drawPixelHandler(canvas: HTMLCanvasElement, setting: { hasLine: 
     // 单点如果点击的位置已经有颜色，且和之前的相同，则清除
     if (rectPosArr[gridX][gridY]?.color === pixelColor) {
       rectPosArr[gridX][gridY] = null
-      clearRect(canvas, {hasLine, x: gridX, y: gridY, gridPerPx, color:pixelColor});
+      clearRect(canvas, {hasLine, x: gridX, y: gridY, gridWidth, gridHeight, color:pixelColor});
     } else {
       rectPosArr[gridX][gridY] = {x:gridX, y:gridY, color:pixelColor }
       reactArr.push({x:gridX, y:gridY})
-      drawRect(canvas, {hasLine, x: gridX, y: gridY, gridPerPx, color:pixelColor});
+      drawRect(canvas, {hasLine, x: gridX, y: gridY, gridWidth, gridHeight, color:pixelColor});
     }
 
   }
@@ -169,16 +175,16 @@ export function drawPixelHandler(canvas: HTMLCanvasElement, setting: { hasLine: 
 
   const onMouseMoveFun = (e: MouseEvent) => {
     if (startMove) {
-      const [gridX, gridY] = mouseToGrid({x:e.offsetX, y:e.offsetY, gridPerPx, hasLine});
-      if (!reactArr.some(item => item.x === gridX && item.y === gridY)) {
+      const [gridX, gridY] = mouseToGrid({x:e.offsetX, y:e.offsetY, gridHeight, gridWidth, hasLine});
 
+      if (!reactArr.some(item => item.x === gridX && item.y === gridY)) {
         // 将坐标信息存储到数组中
         if (!rectPosArr[gridX]) {
           rectPosArr[gridX] = []
         }
         rectPosArr[gridX][gridY] = {x:gridX, y:gridY, color:pixelColor}
         reactArr.push({x:gridX, y:gridY})
-        drawRect(canvas, {hasLine, x: gridX, y: gridY, gridPerPx, color:pixelColor});
+        drawRect(canvas, {hasLine, x: gridX, y: gridY, gridWidth, gridHeight, color:pixelColor});
       }
     }
   }
@@ -189,6 +195,7 @@ export function drawPixelHandler(canvas: HTMLCanvasElement, setting: { hasLine: 
   }
   canvas.addEventListener("mouseup", onMouseUpFun)
 
+  // 将注册事件返回，方便清除事件
   return {
     onMouseDownFun,
     onMouseMoveFun,
@@ -202,26 +209,28 @@ export function drawPixelHandler(canvas: HTMLCanvasElement, setting: { hasLine: 
  * @params { HTMLCanvasElement } canvas 画布对象
  * @params { number } x 第几格
  * @params { number } y 第几列
+ * @params { number } gridWidth 每个格子的像素宽度
+ * @params { number } gridHeight 每个格子的像素高度
  * @params { string } color 矩形颜色
  * 
 */
-export function drawRect(canvas: HTMLCanvasElement, setting: { hasLine: boolean, x:number, y:number, gridPerPx:number, color:string}) {
+export function drawRect(canvas: HTMLCanvasElement, setting: { hasLine: boolean, x:number, y:number, gridWidth:number, gridHeight:number, color:string}) {
   const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
-  const { hasLine, x, y, gridPerPx, color } = setting;
+  const { hasLine, x, y, gridWidth, gridHeight, color } = setting;
   ctx.fillStyle = color;
   if (hasLine) {
     ctx.fillRect(
-      (gridPerPx + 1) * (x - 1),
-      (gridPerPx + 1) * (y - 1),
-      gridPerPx - 1,
-      gridPerPx - 1
+      (gridWidth + 1) * (x - 1),
+      (gridHeight + 1) * (y - 1),
+      gridWidth - 1,
+      gridHeight - 1
     );
   } else {
     ctx.fillRect(
-      (gridPerPx) * (x - 1),
-      (gridPerPx) * (y - 1),
-      gridPerPx,
-      gridPerPx
+      (gridWidth) * (x - 1),
+      (gridHeight) * (y - 1),
+      gridWidth,
+      gridHeight
     );
   }
 }
@@ -235,23 +244,23 @@ export function drawRect(canvas: HTMLCanvasElement, setting: { hasLine: boolean,
  * @params { string } color 矩形颜色
  * 
 */
-export function clearRect(canvas: HTMLCanvasElement, setting: { hasLine: boolean, x:number, y:number, gridPerPx:number, color:string}) {
+export function clearRect(canvas: HTMLCanvasElement, setting: { hasLine: boolean, x:number, y:number, gridWidth:number, gridHeight:number, color:string}) {
   const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
-  const { hasLine, x, y, gridPerPx, color } = setting;
+  const { hasLine, x, y, color, gridWidth, gridHeight } = setting;
   ctx.fillStyle = color;
   if (hasLine) {
     ctx.clearRect(
-      (gridPerPx + 1) * (x - 1),
-      (gridPerPx + 1) * (y - 1),
-      gridPerPx - 1,
-      gridPerPx - 1
+      (gridWidth + 1) * (x - 1),
+      (gridHeight + 1) * (y - 1),
+      gridWidth - 1,
+      gridHeight - 1
     );
   } else {
     ctx.clearRect(
-      (gridPerPx) * (x - 1),
-      (gridPerPx) * (y - 1),
-      gridPerPx,
-      gridPerPx
+      (gridWidth) * (x - 1),
+      (gridHeight) * (y - 1),
+      gridWidth,
+      gridHeight
     );
   }
 }
@@ -261,19 +270,20 @@ export function clearRect(canvas: HTMLCanvasElement, setting: { hasLine: boolean
  * @constructor
  * @params { number } x 鼠标横坐标
  * @params { number } y 鼠标纵坐标
- * @params { number } gridPerPx 每个格子的像素大小
+ * @params { number } gridWidth 每个格子的像素宽度
+ * @params { number } gridHeight 每个格子的像素高度
 */
-export function mouseToGrid(setting:{hasLine: boolean, x: number, y: number, gridPerPx: number}) {
-  const { x, y, gridPerPx, hasLine } = setting;
+export function mouseToGrid(setting:{hasLine: boolean, x: number, y: number, gridWidth: number, gridHeight: number}) {
+  const { x, y, hasLine, gridWidth, gridHeight } = setting;
   if (!hasLine) {
     return [
-      Math.floor(x / gridPerPx) + 1,
-      Math.floor(y / gridPerPx) + 1,
+      Math.floor(x / gridWidth) + 1,
+      Math.floor(y / gridHeight) + 1,
     ];
   } else {
     return [
-      Math.floor(x / (gridPerPx + 1)) + 1,
-      Math.floor(y / (gridPerPx + 1)) + 1,
+      Math.floor(x / (gridWidth + 1)) + 1,
+      Math.floor(y / (gridHeight + 1)) + 1,
     ];
   }
 }
@@ -297,4 +307,59 @@ export function exportCanvasToImg(canvas: HTMLCanvasElement) {
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a); // 下载完成后移除链接元素
+}
+
+/**
+ * 上传图片，将其转化为canvas 
+*/
+export function uploadImgToCanvas(e: any, canvas: HTMLCanvasElement, callback: Function) {
+  const ctx: CanvasRenderingContext2D = canvas.getContext("2d") as CanvasRenderingContext2D;
+  if (e.file) {
+    const reader = new FileReader();
+    reader.onload = function() {
+      const image = new Image()
+      image.src = reader.result as string;
+      image.onload = function() {
+        canvas.width = image.width
+        canvas.height = image.height
+        ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+        if (callback) {
+          callback()
+        }
+      }
+    }
+    reader.readAsDataURL(e.file);
+  }
+}
+
+/**
+ * 将图片数据转换为像素数据
+ * @param canvas 画布
+ * @param setting 转换设置
+ * @param gap 每隔几个像素检测一次，值越大越快，画的像素点越少
+ * @param gridWidth 画的像素宽度
+ * @param gridHeight 画的像素高度
+ */
+export function changeImageDataToPixel(canvas: HTMLCanvasElement, setting: { gap: number , gridWidth: number, gridHeight: number } = { gap: 3, gridWidth: 1, gridHeight: 1 }) {
+  const { gap, gridWidth, gridHeight } = setting
+  const ctx:CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
+  const imageData: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const rectPosArr: ReactPositionInfo[][] = []
+  for (let i = 0; i < imageData.height; i += gap) {
+    rectPosArr[i] = []
+    for (let j = 0; j < imageData.width; j += gap) {
+      // console.log(111)
+      // 当前点位置信息，之所以*4是因为rgba分量是4个字节
+      const position = (imageData.width * i + j) * 4;
+      const color = `rgba(${imageData.data[position]}, ${imageData.data[position + 1]}, ${imageData.data[position + 2]}, ${imageData.data[position + 3] / 255})`
+      rectPosArr[i].push({
+        x: Math.floor(j / gridWidth),
+        y: Math.floor(i / gridHeight),
+        color: color,
+        width: gridWidth,
+        height: gridHeight
+      })
+    }
+  }
+  return {rectPosArr, width: imageData.width , height: imageData.height , gridWidth: gridWidth, gridHeight: gridHeight}
 }
